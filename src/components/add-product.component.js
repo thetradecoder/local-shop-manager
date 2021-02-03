545cf44c2cb907027fb5123f3bc1837c8ca41c6e
thetradecoder <thetradecoder@gmail.com>
2021-02-03 16:38:47 +0600

    local update

--- src/components/add-product.component.js
+++ src/components/add-product.component.js
@@ -5,21 +5,62 @@ import 'jquery/dist/jquery';
 
 
 export default function AddProduct(){
-    const [productId, setProductId] = useState(0);
+    const [productId, setProductId] = useState("");
+    const [productName, setProductName]= useState("");
+    const [companyName, setCompanyName]= useState("");
+    const [brandName, setBrandName] = useState("");
+    const [productGroup, setProductGroup] = useState("");
+
+
     function onChangeProductId(e){
         setProductId(e.target.value);
     }
-    const submitProductData = {
-        productId
+    function onChangeProductName(e){
+        setProductName(e.target.value)
+    }
+    function onChangeCompanyName(e){
+        setCompanyName(e.target.value)
+    }
+    function onChangeBrandName(e){
+        setBrandName(e.target.value)
+    }
+    function onChangeProductGroup(e){
+        setProductGroup(e.target.value)
+    }
+    function onSubmitProductData(e){
+        e.preventDefault();
+        const productData = {
+            productId,
+            productName,
+            companyName,
+            brandName,
+            productGroup
+        }
     }
     return(
         <div>
             <div><h1>Enlist a new product</h1></div>
             <div>
-                <form onSubmit={submitProductData}>
+                <form onSubmit={onSubmitProductData}>
                     <div className="form-group">
                         <label>Product Id</label>
                         <input type="text" onChange={onChangeProductId} className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Product Name</label>
+                        <input type="text" onChange={onChangeProductName} className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Company Name</label>
+                        <input type="text" onChange={onChangeCompanyName} className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Brand Name</label>
+                        <input type="text" onChange={onChangeBrandName} className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Product Group</label>
+                        <input type="text" onChange={onChangeProductGroup} className="form-control"/>
                     </div>
                     <div>
                         <button type="submit" className="btn btn-primary">Add new product</button>
