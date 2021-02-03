383e9c6d3244bad98e05699ba904c734a61f32a0
thetradecoder <thetradecoder@gmail.com>
2021-02-03 17:20:14 +0600

    local update

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/ccs/bootstrap.min.css';
 import AddProduct from './components/add-product.component';
+import SideNav from './components/side-nav.component';
 
 function App() {
   return (
     <Router>
+      <SideNav/>
       <Route path="/add-product" component={AddProduct}/>
     </Router>
   );
