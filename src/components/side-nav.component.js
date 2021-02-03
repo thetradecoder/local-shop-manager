99bb32d1c2ddb342858fbd6c7d1148c8860d0901
thetradecoder <thetradecoder@gmail.com>
2021-02-03 17:18:53 +0600

    local update

---
+++ src/components/side-nav.component.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "jquery/dist/jquery.js";
+import "../App.css";
+
+export default function SideNav(){
+    const user = localStorage.getItem('user');
+
+    function loginLogout(){
+        if(user){
+            return(
+                <div>Logout</div>
+            );
+        }else{
+            return(
+                <div>Login</div>
+            );
+        }
+    }
+    function loginLogoutLink(){
+        if(user){
+            return "/accounts/logout/";
+        }else{
+            return "/";
+        }
+    }
+
+    function renderSideNav(){
+        if(user){
+            return(
+                <div className="side-nav-container shadow">
+            
+            <div className="side-nav">
+                <div>
+                    <h3>Shop Manager</h3>
+                    <p>By: TradeCoder</p>
+                </div>
+                <div>
+                    <Link to={loginLogoutLink} className="form-control nav-link">{loginLogout()}</Link>             
+                    <Link to="/add-product/" className="form-control nav-link">Add a Product</Link>                    
+                </div>
+            </div>
+        </div>
+            );
+        }
+    }
+
+    return (
+        <div>
+            {renderSideNav()}
+        </div>
+        
+    );
+
+}
