0e5ad974a7c6a9045ef1d57ce1c4d58ac959916c
thetradecoder <thetradecoder@gmail.com>
2021-02-04 22:49:54 +0600

    working on add-product router

--- server/routes/add-product.route.js
+++ server/routes/add-product.route.js
@@ -1,3 +1,14 @@
 const router = require('express').Router();
 const AddProduct = require('../models/add-product.model');
 
+router.route('/add-product')
+.post((req, res)=>{
+    const {username, userid}=req.params;
+    const {productId, productName, companyName, brandName, productGroup}= req.body;
+    const productData = {productId, productName, companyName, brandName, productGroup};
+    productData.save()
+    .then(data=>res.send(data))
+    .catch(err=>res.send('Operation Failed!'+err))
+});
+
+module.exports =  router;
