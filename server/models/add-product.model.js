029b00bebd2a967ab933b158cfc51c9f9576d86d
thetradecoder <thetradecoder@gmail.com>
2021-02-05 22:25:56 +0600

    local update

--- server/models/add-product.model.js
+++ server/models/add-product.model.js
@@ -18,9 +18,39 @@ const ProductSchema = new Schema({
             remarks:{type:String}
         }
     ],
-    salesHistory:[],
-    buyOrderPipeline:{type:Number},
-    salesOrderPipline:{type:Number},
+    salesHistory:[
+        {
+            date:{type:Date},
+            quantity:{type:Number},
+            unitePrice:{type:Number},
+            totalPrice:{type:Number},
+            payment:{type:String},
+            remarks:{type:String},
+            customer:{type:String}
+        }
+    ],
+    buyOrderPipeline:[
+        {
+            orderDate:{type:Date},
+            quantity:{type:Number},
+            unitePrice:{type:Number},
+            totalPrice:{type:Number},
+            orderBy:{type:String},
+            orderTakenBy:{type:String},
+            remarks:{type:String}
+        }
+    ],
+    salesOrderPipline:[
+        {
+            orderDate:{type:Date},
+            quantity:{type:Number},
+            unitePrice:{type:Number},
+            totalPrice:{type:Number},
+            orderBy:{type:String},
+            orderTakenBy:{type:String},
+            remarks:{type:String}
+        }
+    ],
     totalBuyValue:{type:Number},
     totalSalesValue:{type:Number},
     username:{type:String},
