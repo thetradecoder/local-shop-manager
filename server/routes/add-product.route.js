909194b2f306f0d640d149c06a0b1d676db58010
thetradecoder <thetradecoder@gmail.com>
2021-02-06 23:10:09 +0600

    local update

--- server/routes/add-product.route.js
+++ server/routes/add-product.route.js
@@ -13,7 +13,7 @@ router.route('/add-product')
 
 rotuer.route('/show-product-list')
 .get((req, res)=>{
-    Product.find({}, {productId:true, productName:true})
+     Product.find({}, {productId:true, productName:true})
     .then(data=>res.send(data))
     .catch(err=>res.send(err))
 });
@@ -30,6 +30,15 @@ router.route('/show-single-product-position')
 .get((req, res)=>{
     Product.findOne({productId}, {productId:true, productName:true, companyName:true, brandName:true, productGroup:true,  stock:true, totalBuyValue:true, totalSalesValue:true})
     .then(data=>res.send(data))
+    .catch(err=>res.send(err))
+});
+
+router.route('/buy-a-product')
+.put((req, res)=>{
+    const {username, userid, productId} =  req.params;
+    const {buyHistory} = req.body;
+    Product.findOneAndUpdate({productId, username, userid}, {$push:{buyHistory}, $inc:{stock:buyHistory.quantity, totalBuyValue:buyHistory.totalPrice}})
+    .then(data=>res.status(200).send('Buy completed!'))
     .catch(err=>res.send(err))
 })
 
