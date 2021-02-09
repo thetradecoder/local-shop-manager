469c09a5a6756b2bb1b660fbaba582fb87326e14
thetradecoder <thetradecoder@gmail.com>
2021-02-09 16:21:54 +0600

    local update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddProduct from './components/add-product.component';
 import SideNav from './components/side-nav.component';
 import UserLogin from './components/login.component';
 import Logout from './components/logout.component';
+import UserForm from './components/signup.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <SideNav/>
       <Route path="/" component={UserLogin} exact/>
       <Route path ="/accounts/logout" component={Logout} />
+      <Route path = "/accounts/signup" component={UserForm}/>
       <Route path="/add-product" component={AddProduct}/>
     </Router>
   );
