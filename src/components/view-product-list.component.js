0f96ab905bb464efb60d2e474a4523e5c8d806c4
thetradecoder <thetradecoder@gmail.com>
2021-02-10 10:36:19 +0600

    working on view-product-list component

---
+++ src/components/view-product-list.component.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'jquery/dist/jquery';
+import '../App.css';
+
+import axios from 'axios';
+
+export default function ViewProductList(){
+    return(
+        <div>
+            <div>
+                <h1>Product List</h1>
+            </div>
+        </div>
+    );
+}
