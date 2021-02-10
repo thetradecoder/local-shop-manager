443e37e38ff85e5af0bd7c6e02d0b375302ffa78
thetradecoder <thetradecoder@gmail.com>
2021-02-10 10:50:15 +0600

    update view product

--- src/components/view-product-list.component.js
+++ src/components/view-product-list.component.js
@@ -1,12 +1,28 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'jquery/dist/jquery';
 import '../App.css';
 
 import axios from 'axios';
+const username = localStorage.getItem('username');
+const userid = localStorage.getItem('userid');
 
 export default function ViewProductList(){
+    const [productName, setProductName] = useState();
+    useEffect(()=>{
+        axios.get(`http://localhost:5000/view-product-list/`${username}/${userid})
+        .then(d=>d.map((e,i)=>{
+            return(
+                <tr>
+                    <td>{e.productId}</td>
+                    <td>{e.productName}</td>
+                    <td>{e.brand}</td>
+                    <td>{e.stock}</td>
+                </tr>
+            );
+        }))
+    })
     return(
         <div>
             <div>
