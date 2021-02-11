200a631ff4e60fbae5c328f90f58ef339cd78e9f
thetradecoder <thetradecoder@gmail.com>
2021-02-11 21:21:37 +0600

    local update

--- src/components/view-product-list.component.js
+++ src/components/view-product-list.component.js
@@ -9,31 +9,23 @@ const username = localStorage.getItem('username');
 const userid = localStorage.getItem('userid');
 
 export default function ViewProductList(){
-    const [productName, setProductName] = useState();
+    const [productList, setProductList] = useState([]);
 
-    function List(){
-    useEffect(()=>{
+
+    useEffect(){
         axios.get(`http://localhost:5000/view-product-list/${username}/${userid}`)
-        .then(d=>d.map((e,i)=>{
-            return(
-                <tr>
-                    <td>{e.productId}</td>
-                    <td>{e.productName}</td>
-                    <td>{e.brand}</td>
-                    <td>{e.stock}</td>
-                </tr>
-            );
-        }))
+        .then(d=>setProductList(d))
         .catch(err=>window.alert(err))
-    })
-}
+    }
+
+ 
     return(
         <div>
             <div>
                 <h1>Product List</h1>
             </div>
             <table>
-               {List()}
+              
             </table>
         </div>
     );
