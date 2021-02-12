d75ff6a144a89a77b02ac1fe40849f3f1dd7a960
thetradecoder <thetradecoder@gmail.com>
2021-02-12 22:44:54 +0600

    local update

--- src/components/view-stock.component.js
+++ src/components/view-stock.component.js
@@ -10,6 +10,29 @@ const userid = localStorage.getItem('userid');
 
 
 export default function ViewStock(){
+    const [product, setProduct]= useState({});
+    const [stock, setStock] = useState(0);
+
+
+    useEffect(()=>{
+        axios.get(`http://localhost:5000/view-stock/${productId}/${username}/${userid}`)
+        .then(d=>setProduct(d.data))
+        .catch(err=>window.alert(err))
+    });
+
+    const productDetails = ()=>{
+        return(
+            <div>
+                <p>Stock position Date and Time: {new Date()}</p>
+                <p>Product Name: {product.productId}-{product.productName}</p>
+                <p>Brand Name: {product.brandName}-{product.companyName}</p>
+                <p>Stock position: {product.stock}</p>
+
+            </div>
+        );
+    }
+
+
     return(
         <div></div>
     );
