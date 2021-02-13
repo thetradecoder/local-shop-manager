5ec1064fb83a8d9c6c9f283d182be79daba504b8
thetradecoder <thetradecoder@gmail.com>
2021-02-13 21:08:39 +0600

    local update

--- src/components/view-stock.component.js
+++ src/components/view-stock.component.js
@@ -17,40 +17,10 @@ export default function ViewStock(){
         setProductId(e.target.value);
     }
 
-    function onSubmitStockQuery(e){
-        e.preventDefault();
-
-        axios.get(`http://localhost:5000/view-stock/${productId}/${username}/${userid}`)
-        .then(d=>setProduct(d.data))
-        .catch(err=>window.alert(err))
-    }
-    
-
-
-
-    const productDetails = ()=>{
-        return(
-            <div>
-                <form onSubmit={onSubmitStockQuery}>
-                    <div>
-                        <label>ProductId</label>
-                        <input type="text" onChange={onChangeProductId} className="form-control"/>
-                    </div>
-                    <button type="submit" className="btn btn-primary">View Stock</button>
-                </form>
-                <p>Stock position Date and Time: {new Date()}</p>
-                <p>Product Name: {product.productId}-{product.productName}</p>
-                <p>Brand Name: {product.brandName}-{product.companyName}</p>
-                <p>Stock position: {product.stock}</p>
-
-            </div>
-        );
-    }
-
-
+  
     return(
         <div>
-            {productDetails()}
+          
         </div>
     );
 }
