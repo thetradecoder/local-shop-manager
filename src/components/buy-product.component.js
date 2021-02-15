3d40cd0a4f5ee35181ddacc94579489dc2257381
thetradecoder <thetradecoder@gmail.com>
2021-02-15 17:00:50 +0600

    local update

---
+++ src/components/buy-product.component.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'jquery/dist/jquery';
+import '../App.css';
+
+const username = localStorage.getItem('username');
+const userid = localStorage.getItem('userid');
+
+export default function Buy(){
+    return(
+        <div>
+            
+        </div>
+    );
+}
