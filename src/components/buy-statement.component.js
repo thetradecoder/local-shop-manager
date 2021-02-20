c9771d182d5331219533c7793584b817d4f25d23
thetradecoder <thetradecoder@gmail.com>
2021-02-20 23:46:32 +0600

    local update

---
+++ src/components/buy-statement.component.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'jquery/dist/jquery';
+import '../App.css';
+
+
+const username = localStorage.getItem('username');
+const userid = localStorage.getItem('userid');
+
+export default function BuyStatement(){
+    return(
+        <div>
+
+        </div>
+    );
+}
